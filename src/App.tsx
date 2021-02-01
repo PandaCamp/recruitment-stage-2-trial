c1804da2e38c2eb0438918f2f34dfe4526aa9b2a
noberk <noberk@users.noreply.github.com>
2021-02-01 11:21:12 +0800

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Login from './pages/Login'
 import SliderLoadList from './pages/List'
 function App() {
-  // return <Login />
-  return <SliderLoadList />
+  return <Login />
+  // return <SliderLoadList />
 }
 
 export default App
