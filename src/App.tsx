8a632bff4ed64dbef7766df0eb21f182e7a7ad32
noberk <noberk@users.noreply.github.com>
2021-02-03 23:28:49 +0800

    add a new banner

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Login from './pages/Login'
 import SliderLoadList from './pages/List'
 import PDF from './pages/PDFViewer'
 function App() {
-  return <PDF />
-  // return <SliderLoadList />
+  // return <PDF />
+  return <SliderLoadList />
 }
 
 export default App
