2b2b7a9e93c0837c860f6c1b8d2a980880d1cdd2
noberk <noberk@users.noreply.github.com>
2021-02-04 00:15:33 +0800

    oversee

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,7 @@
 import React, {FC, useCallback, useEffect, useLayoutEffect, useRef, useState} from 'react'
 import Button from '../components/button'
 import {useList} from '../hooks/useList'
-
+import s from '../assets/banner1.jpg'
 import './list.css'
 const btn: React.CSSProperties = {
   marginTop: 50,
@@ -23,9 +23,13 @@ interface ListProps {
 }
 const SliderLoadList: FC<Partial<ListProps>> = () => {
   const [value, setValue] = useState(0)
+  const [isHeaderVisible, setIsHeaderVisible] = useState<boolean>(true)
+
   const {hasMore, loading, list, error} = useList(+value, 10)
 
   const observer = useRef<any>()
+  const headerObserver = useRef<any>()
+
   const lastItemRef = useCallback(
     (node: any) => {
       if (loading) return
@@ -39,18 +43,35 @@ const SliderLoadList: FC<Partial<ListProps>> = () => {
     },
     [loading, hasMore]
   )
+  const headerRef = useCallback((node: any) => {
+    if (headerObserver.current) headerObserver.current.disconnect()
+    headerObserver.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        console.log('see')
+      } else {
+        console.log('blink')
+      }
+    })
 
+    if (node) headerObserver?.current?.observe(node)
+  }, [])
+  useEffect(() => {
+    console.log(isHeaderVisible)
+  }, [isHeaderVisible])
   return (
     <div>
       <header
+        ref={headerRef}
         style={{
-          background: 'RED',
-          height: 80,
+          height: 160,
         }}>
-        list information
+        <img src={s} alt="" style={{height: 160, width: '100%'}} />
       </header>
-      <input type="text" value={value} onChange={(e: any) => setValue(e.target.value)} />
-      <button onClick={() => setValue((value) => value + 1)}>+</button>
+      <div style={{height: 100, background: 'gray'}}>
+        <input type="text" value={value} onChange={(e: any) => setValue(e.target.value)} />
+        <button onClick={() => setValue((value) => value + 1)}>+</button>
+      </div>
+
       <article>
         {list.map((i: any, index: number, arr) => {
           if (arr.length - 1 === index) {
