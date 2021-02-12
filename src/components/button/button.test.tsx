c96cc57235ce611623729e075cf877041dc74a03
noberk <noberk@users.noreply.github.com>
2021-02-12 17:22:06 +0800

    asss

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import Button from '.'
 
 import {render} from '@testing-library/react'
+import renderer from "react-test-renderer"
+
 it('rende', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Button value="123" />, div)
@@ -11,4 +13,11 @@ it('rende', () => {
 it('render button correctly', () => {
   const {getByTestId} = render(<Button value="123" />)
   expect(getByTestId('button')).toHaveTextContent('123')
+})
+
+
+it('match snapshot 1',()=>{
+    const tree = renderer.create(<Button value="~~~" />).toJSON()
+    
+
 })
