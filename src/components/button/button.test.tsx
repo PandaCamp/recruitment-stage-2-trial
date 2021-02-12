ac84bf3ed880629d3d8d6a1fe0f21a0a16f5c01f
noberk <noberk@users.noreply.github.com>
2021-02-12 17:10:12 +0800

    sss

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from '.'
 
+import {render} from '@testing-library/react'
 it('rende', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Button value="123" />, div)
+})
+
+it('render button correctly', () => {
+  const {getByTestId} = render(<Button value="123" />)
+  expect(getByTestId('button')).toHaveTextContent('123')
 })
